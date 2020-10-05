28fa4be6df6ab2e8b0d455b08336e448acc611b7
unknown <tathagat.dalai@gmail.com>
2020-10-06 01:15:27 +0530

    intial8 commit

--- dentist/settings.py
+++ dentist/settings.py
@@ -128,11 +128,12 @@ STATICFILES_DIRS=[
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 #Email setings
+EMAIL_BACKEND='django.core.mail.backend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_HOST_USER='tathagat.dalai@gmail.com'
 EMAIL_HOST_PASSWORD='tathagat1234'
-EMAIL_USE_TLS=True#
+EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
 
